fb4fc7d1a179fef10ba362d0d64879fbd509eef4
Stephen <sctwomey@gmail.com>
2017-04-21 11:37:48 -0700

    added 03_variables files

---
+++ 03_variables/03_less-emphasis/08_exercise_your-name/02_anotherSolution/myNameVar.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "Steve"
+	fmt.Println("Hello ", name)
+}
---
+++ 03_variables/03_less-emphasis/08_exercise_your-name/03_anotherSolution/myNameVar.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Steve"
+	fmt.Println("Hello ", name)
+}
