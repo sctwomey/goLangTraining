29747a6942535ae5c0b1aaaa250ceeb777695934
Stephen <sctwomey@gmail.com>
2017-04-22 15:16:48 -0700

    removes escape characters in fully qualified name

--- 04_scope/01_package-scope/02_visibility/main/main.go
+++ 04_scope/01_package-scope/02_visibility/main/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com\sctwomey\goLangTraining\04_scope\01_package-scope\02_visibility\vis"
+	"github.com/sctwomey/goLangTraining/04_scope/01_package-scope/02_visibility/vis"
 )
 
 
