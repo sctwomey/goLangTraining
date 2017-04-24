45bdf3d86af5f6d41970e1dbe6e536af665f4b3b
Stephen <sctwomey@gmail.com>
2017-04-23 18:21:10 -0700

    adds files for 05_blank-identifier & 06_constants

---
+++ 05_blank-identifier/02_http-get_example/02_no-error-checking/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+	"io/ioutil"
+	"fmt"
+)
+
+func main() {
+	res, _ := http.Get("http://www.mcleods.com/")
+	page, _ := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	fmt.Printf("%s", page)
+}
+
+
---
+++ 06_constants/02_multiple-initialization/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const (
+	Pi 	 = 3.14
+	Language = "Go"
+)
+
+func main() {
+	fmt.Println("PI =", Pi)
+	fmt.Println("Language =", Language)
+}
